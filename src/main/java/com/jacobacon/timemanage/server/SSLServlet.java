c10c4f2238771223fb97c637a2f83bb278a0290a
Jacob Beneski <jacobbeneski@gmail.com>
2016-10-07 00:18:59 -0500

    Enable SSL by Default

---
+++ src/main/java/com/jacobacon/timemanage/server/SSLServlet.java
@@ -0,0 +1,23 @@
+package com.jacobacon.timemanage.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SSLServlet extends HttpServlet{
+	
+public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("cEvPlfr1TqVL_D5QYNVRgepSNBlb7URZp4PqS5oM0zo.jPVLF3hkPkrS7ioB4GQqQyNOXoxopB11oSkpMHckIp0");
+		
+	}
+
+}
