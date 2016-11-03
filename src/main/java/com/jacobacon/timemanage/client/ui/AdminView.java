b61d2cbf94eb18f384ed846b26c894d5ed57bb94
Jacob Beneski <jacobbeneski@gmail.com>
2016-11-03 11:23:51 -0500

    Sart Admin Panel

--- src/main/java/com/jacobacon/timemanage/client/ui/AdminView.java
+++ src/main/java/com/jacobacon/timemanage/client/ui/AdminView.java
@@ -1,47 +1,23 @@
 package com.jacobacon.timemanage.client.ui;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 
-public class AdminView extends Composite implements HasText {
+public class AdminView extends Composite{
 
 	private static AdminViewUiBinder uiBinder = GWT.create(AdminViewUiBinder.class);
 
 	interface AdminViewUiBinder extends UiBinder<Widget, AdminView> {
 	}
+	
+	
 
 	public AdminView() {
 		initWidget(uiBinder.createAndBindUi(this));
 		Window.setTitle("Admin");
-	}
-
-	@UiField
-	Button button;
-
-	public AdminView(String firstName) {
-		initWidget(uiBinder.createAndBindUi(this));
-		button.setText(firstName);
-	}
-
-	@UiHandler("button")
-	void onClick(ClickEvent e) {
-		Window.alert("Hello!");
-	}
-
-	public void setText(String text) {
-		button.setText(text);
-	}
-
-	public String getText() {
-		return button.getText();
 	}
 
 }
