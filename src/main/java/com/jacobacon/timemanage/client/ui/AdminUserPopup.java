11a6fac8fba1b9cd53a4d68f911bde5e86d5efd3
Jacob Beneski <jacobbeneski@gmail.com>
2016-11-26 11:14:04 -0600

    Start Popup Work

--- src/main/java/com/jacobacon/timemanage/client/ui/AdminUserPopup.java
+++ src/main/java/com/jacobacon/timemanage/client/ui/AdminUserPopup.java
@@ -3,15 +3,12 @@ package com.jacobacon.timemanage.client.ui;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 
-public class AdminUserPopup extends Composite implements HasText {
+public class AdminUserPopup extends Composite {
 
 	private static AdminUserPopupUiBinder uiBinder = GWT.create(AdminUserPopupUiBinder.class);
 
@@ -22,25 +19,9 @@ public class AdminUserPopup extends Composite implements HasText {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
-	@UiField
-	Button button;
-
-	public AdminUserPopup(String firstName) {
-		initWidget(uiBinder.createAndBindUi(this));
-		button.setText(firstName);
-	}
-
-	@UiHandler("button")
+	@UiHandler("saveButton")
 	void onClick(ClickEvent e) {
 		Window.alert("Hello!");
-	}
-
-	public void setText(String text) {
-		button.setText(text);
-	}
-
-	public String getText() {
-		return button.getText();
 	}
 
 }
